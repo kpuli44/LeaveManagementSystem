27f566932f2397906b766960b5bd940f61596c63
kisho <kisho@DESKTOP-DS6GB3O>
2024-12-16 18:44:47 -0600

    Add project files.

---
+++ LeaveManagementSystem.Web/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using LeaveManagementSystem.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystem.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel()
+            {
+                Name = "Student of ASP.NET Core Mastery",
+                DateOfBirth = new System.DateTime(1987, 4, 4)
+            };
+
+            return View(data);
+        }
+    }
+}
